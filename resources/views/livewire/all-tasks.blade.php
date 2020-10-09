25807de4e4c57a1f678155d5b25861a56517e7de
Chiedu Emmanuel <iamnedu912@gmail.com>
2020-10-09 04:24:52 +0100

    made view responsive

--- resources/views/livewire/all-tasks.blade.php
+++ resources/views/livewire/all-tasks.blade.php
@@ -1,22 +1,25 @@
-<div class="mt-2">
-    @foreach($tasks as $task)
-    <div class="flex w-9/12 justify-between">
-        <ul class="mr-4 mb-1">
-            <li class="{{ $task->is_complete ? 'line-through list-none' : 'list-none' }}">
-                <input type="checkbox" {{ $task->is_complete ? 'checked' : '' }} class="mr-2 cursor-pointer" wire:change="toggleComplete({{ $task->id }})" id="">
-                <a href="#" class="hover:no-underline" wire:click="$emit('showTask',{{ $task->id }})">{{ $task->task }}</a>
-            </li>
-        </ul>
-        <ul class="mb-1">
-            <li class="list-none">
-                <x-jet-danger-button 
-                    onClick="confirm('Are you sure?') || event.stopImmediatePropagation()"
-                    wire:click="deleteTask({{ $task->id }})" 
-                >
-                    Delete
-                </x-jet-danger-button>
-            </li>
-        </ul>
+<div class="mt-2 p-4">
+    <div class="sm:w-full md:w-9/12">
+        @foreach($all_tasks as $task)
+        <div class="flex justify-between mb-2">
+            <ul class="mr-4 mb-1">
+                <li class="{{ $task->is_complete ? 'line-through list-none' : 'list-none' }}">
+                    <input type="checkbox" {{ $task->is_complete ? 'checked' : '' }} class="mr-2 cursor-pointer" wire:change="toggleComplete({{ $task->id }})" id="">
+                    <a href="#" class="hover:no-underline" wire:click="$emit('showTask',{{ $task->id }})">{{ $task->task }}</a>
+                </li>
+            </ul>
+            <ul class="mb-1">
+                <li class="list-none">
+                    <x-jet-danger-button 
+                        onClick="confirm('Are you sure?') || event.stopImmediatePropagation()"
+                        wire:click="deleteTask({{ $task->id }})" 
+                    >
+                        Delete
+                    </x-jet-danger-button>
+                </li>
+            </ul>
+        </div>
+        @endforeach
+        {{ $all_tasks->links() }}
     </div>
-    @endforeach
 </div>
